0959169c3c945382a2d9350f629aea03435a152d
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-15 16:25:04 -0300

    Adicionadas varias funções

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-function randomColor (){
-    const color = `rgb(${Math.ceil(Math.random()*255)} , ${Math.ceil(Math.random()*255)} , ${Math.ceil(Math.random()*255)})`;
-    return color;
+document.getElementById('answer').innerText = 'Escolha uma cor';
+document.querySelector('#reset-game').addEventListener('click', () => {
+  ballColor();
+  randomBall();
+  document.querySelector('#answer').innerText = 'Escolha uma cor';
+});
+
+function randomColor() {
+  const color = `rgb(${Math.ceil(Math.random() * 255)} , ${Math.ceil(
+    Math.random() * 255
+  )} , ${Math.ceil(Math.random() * 255)})`;
+  return color;
 }
 randomColor();
 
-document.querySelectorAll('.ball').forEach((item) => {
+function ballColor() {
+  document.querySelectorAll('.ball').forEach((item) => {
+    item.classList.remove('premiado');
     item.style.backgroundColor = randomColor();
-})
-
+  });
+}
+ballColor();
+document.querySelectorAll('.ball').forEach((item) => {
+  if (item.classList == 'selecionado') {
+    document.querySelector('p').innerText = item.style.backgroundColor;
+  }
+});
+randomBall();
+function randomBall() {
+  document.querySelectorAll('.ball').forEach((item) => {
+    if (item.classList[1] === Math.ceil(Math.random() * 6).toString()) {
+      item.classList.add('premiado');
+      const colorName = document.querySelector('.premiado').style
+        .backgroundColor;
+      document.querySelector('#rgb-color').innerText = colorName.slice(3);
+    }
+  });
+}
+document.querySelectorAll('.ball').forEach((item) => {
+  item.addEventListener('click', () => {
+    if (item.classList[2] === 'premiado') {
+      return (document.querySelector('#answer').innerText = 'Acertou!');
+    }
+    document.querySelector('#answer').innerText = 'Errou! Tente novamente!';
+  });
+});
+//
 // TODO: Criar função com if item backgroundcolor == p adiciona a classe resposta certa, depois criar outra função que pega a classe das bolas, se a classe for acertou imprimir a mensagem acertou e se errar a mensagem errou
