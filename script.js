94e8003562f3de5758cd3505dc20dbbb1493d1c9
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-15 14:26:18 -0300

    Adicionando o evento click aos elementos balls

--- script.js
+++ script.js
@@ -10,7 +10,30 @@ function colorGenerator() {
   const balls = document.querySelector('.balls-container').children;
   for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = rgbGenerator();
-  };
+  }
+  choseColorAnswer(balls);
 }
+
+function choseColorAnswer(ballsArray) {
+  const sortedNum = Math.floor(Math.random() * 6);
+  const rgbColorElement = document.querySelector('#rgb-color');
+  const simpleString = ballsArray[sortedNum].style.backgroundColor.slice(3);
+  rgbColorElement.innerText = simpleString;
+}
+
+const ballsContainer = document.querySelector('.balls-container');
+
+ballsContainer.addEventListener('click', function (event) {
+  const answerElement = document.querySelector('#answer');
+  if (event.target.className == 'ball') {
+    const rgbColorElement = document.querySelector('#rgb-color').innerText;
+    if (event.target.style.backgroundColor.includes(rgbColorElement)) {
+      answerElement.innerText = 'Acertou!'
+    }
+    else {
+      answerElement.innerText = 'Errou! Tente novamente!'
+    }
+  }
+})
 
 window.onload = colorGenerator;
