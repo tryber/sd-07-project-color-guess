aa5c48e5039f9f0caabb8dbe2868691387f033ce
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-15 17:27:07 -0300

    Requirement Bonus 07

--- script.js
+++ script.js
@@ -2,68 +2,69 @@ const rgbColorText = document.getElementById('rgb-color');
 const answerText = document.getElementById('answer');
 const ballList = document.querySelectorAll('.ball');
 const buttonReset = document.getElementById('reset-game');
+const scoreText = document.getElementById('score');
+const winIcrease = 3;
+
 // set up game messages
-const msgList = { 
-	begin: 'Escolha uma cor',
-	win: 'Acertou!', 
-	loose: 'Errou! Tente novamente!'
- };
+const msgList = {
+  begin: 'Escolha uma cor',
+  win: 'Acertou!',
+  loose: 'Errou! Tente novamente!'
+};
 
 // assign function initGame to onload
 window.onload = initGame;
 
 // function that run after page on load
 function initGame() {
-	// generate color rgb
-	rgbColorText.innerText = generateRandomRGB();
-	
-	// set up begin msg	
-	answerText.innerText = msgList.begin;
+  // generate color rgb
+  rgbColorText.innerText = generateRandomRGB();
 
-	// fill ball with a random color
-	fillBalls();
+  // set up begin msg
+  answerText.innerText = msgList.begin;
+
+  // fill ball with a random color
+  fillBalls();
 }
 
 
 // function to generate random color RGB
 function generateRandomRGB() {
-	// generate random numbers [1]
-	const numberR = Math.floor(Math.random() * 256);
-	const numberG = Math.floor(Math.random() * 256);
-	const numberB = Math.floor(Math.random() * 256);
-	// build the result text
-	const resultRGB = `(${numberR}, ${numberG}, ${numberB})`;
-	// return the result
-	return resultRGB;
+  // generate random numbers [1]
+  const numberR = Math.floor(Math.random() * 256);
+  const numberG = Math.floor(Math.random() * 256);
+  const numberB = Math.floor(Math.random() * 256);
+  // build the result text
+  const resultRGB = `(${numberR}, ${numberG}, ${numberB})`;
+  // return the result
+  return resultRGB;
 }
 
 // function fill balls with random color
 function fillBalls() {
-	// for each ball
-	for (let index = 0; index < ballList.length; index += 1) {
-		ballList[index].style.backgroundColor = 'rgb' + generateRandomRGB();
-	}
-	// random choose one, and put the text color [0-5]
-	const randomBall = Math.floor(Math.random() * 6);
-	ballList[randomBall].style.backgroundColor = 'rgb' + rgbColorText.innerText;
-	// log
-	console.log(randomBall)
-	console.log(ballList[randomBall].style.backgroundColor)
+  // for each ball
+  for (let index = 0; index < ballList.length; index += 1) {
+    ballList[index].style.backgroundColor = 'rgb' + generateRandomRGB();
+  }
+  // random choose one, and put the text color [0-5]
+  const randomBall = Math.floor(Math.random() * 6);
+  ballList[randomBall].style.backgroundColor = 'rgb' + rgbColorText.innerText;
 }
 
 // function hit the color ball
 function hit(event) {
-	// Get the ball that throw the event
-	const ballClick = event.target;
-	console.log(ballClick.style.backgroundColor,' == ','rgb' + rgbColorText.innerText);
-	// Compare the color ballClick with text
-	if (ballClick.style.backgroundColor == 'rgb' + rgbColorText.innerText) {
-		// Show the win message
-		answerText.innerText = msgList.win;
-	} else {
-		// Show the loose message
-		answerText.innerText = msgList.loose;
-	}
+  // Get the ball that throw the event
+  const ballClick = event.target;
+  // Compare the color ballClick with text
+  if (ballClick.style.backgroundColor == 'rgb' + rgbColorText.innerText) {
+    // Show the win message
+    answerText.innerText = msgList.win;
+    // increase points with 3
+    scoreText.innerText = '' + (parseInt(scoreText.innerText) + winIcrease);
+  } else {
+    // Show the loose message
+    answerText.innerText = msgList.loose;
+  }
 }
 
 // Assign click button to function initGame
@@ -71,5 +72,5 @@ buttonReset.addEventListener('click', initGame);
 
 // Assign hit function to all balls
 for (let index = 0; index < ballList.length; index += 1) {
-	ballList[index].addEventListener('click', hit);
+  ballList[index].addEventListener('click', hit);
 }
