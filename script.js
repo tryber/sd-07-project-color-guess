5152d2561a2786262e8e106bc286954c7ab6a26f
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-15 17:16:00 -0300

    adicionando cores aleatorias nos circulos

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const randomRgbElement = document.getElementById('rgb-color');
-
-// numeros aleatorios rgb
-const randomR = Math.round(Math.random() * 255);
-const randomG = Math.round(Math.random() * 255);
-const randomB = Math.round(Math.random() * 255);
+const randomRgbToGuess = document.getElementById('rgb-color');
+const ballsElementsList = document.getElementsByClassName('ball');
+let rgbStringList = [];
 
 function generateRandomRgb() {
+  const randomR = Math.round(Math.random() * 255);
+  const randomG = Math.round(Math.random() * 255);
+  const randomB = Math.round(Math.random() * 255);
   return `(${randomR}, ${randomG}, ${randomB})`
 }
 
-randomRgbElement.innerText = generateRandomRgb();
+// function that shuffles the array (borrowed from stack overflow)
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    }
+  return array;
+}
+
+// generate the right number and adds to the page and the list
+const colorToGuess = generateRandomRgb();
+rgbStringList.push(colorToGuess);
+randomRgbToGuess.innerText = colorToGuess;
+
+// generate five random numbers and append to the list
+for (let i = 0; i < 5; i += 1) {
+  rgbStringList.push(generateRandomRgb());
+}
+
+// shuffling the array and add the rgb values to the circles
+const newRgbArray = shuffleArray(rgbStringList);
+console.log(ballsElementsList[0]);
+for (let j = 0; j < newRgbArray.length; j += 1) {
+  ballsElementsList[j].style.backgroundColor = "rgb" + newRgbArray[j];
+}
