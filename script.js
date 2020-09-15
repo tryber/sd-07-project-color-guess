df8bc4e01d693990cfc2c35af27ec7c33afcad7d
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-15 13:58:07 -0300

    Feito uma lista TODO dentro do javascript, pausa para o momento inicial

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ randomColor();
 document.querySelectorAll('.ball').forEach((item) => {
     item.style.backgroundColor = randomColor();
 })
+
+// TODO: Criar função com if item backgroundcolor == p adiciona a classe resposta certa, depois criar outra função que pega a classe das bolas, se a classe for acertou imprimir a mensagem acertou e se errar a mensagem errou
