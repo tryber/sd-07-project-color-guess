9d96a59fa42984c557044fd8ed77034dd26867d8
Pedro MF <pedromarques1011@gmail.com>
2020-09-16 18:13:20 -0300

    req 3

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const OUTPUT_COLOR = document.querySelector(".output");
 
 function randomNumber() {
-  return Math.ceil(Math.random() * 255);
+  const maxNumber = 255;
+  return Math.ceil(Math.random() * maxNumber);
 }
 
 OUTPUT_COLOR.innerText = `(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+
+const balls = document.querySelectorAll(".ball");
+for (let index = 0; index < balls.length; index += 1) {
+  balls[index].style.backgroundColor = `rgb(
+    ${randomNumber()},
+    ${randomNumber()},
+    ${randomNumber()})`;
+}
