5b53552a2d6aaa9b34c41961b10882e78bf4fc1a
moacyrrln <moacyrrln@gmail.com>
2020-09-16 19:55:53 -0300

    200916195553

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-document.getElementById("rgb-color").innerText = "("+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
+let buttonreset = document.getElementById("reset-game");
+buttonreset.addEventListener("click",reset);
+reset();
 
-console.log(Math.random*255);
+function reset() {
+    document.getElementById("answer").innerText = "Escolha uma cor";
+    let colorrandom = "("+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
+    document.getElementById("rgb-color").innerText = colorrandom;
+    document.getElementById("rgb-color").style.backgroundColor = "rgb"+colorrandom;
+    let balls = document.getElementsByClassName("ball");
+    let random = parseInt(Math.random()*6);
+    for (let i = 0;i < balls.length;i++) {
+        if (i != random) {
+            balls[i].style.backgroundColor = "rgb("+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
+        } else {
+            balls[i].style.backgroundColor = "rgb"+colorrandom;
+        }
+        balls[i].addEventListener("click", function(event) {
+            if (event.target == balls[random]) {
+                document.getElementById("answer").innerText = "Acertou!";
+            } else {
+                document.getElementById("answer").innerText = "Errou! Tente novamente!";
+            }
+        })
+    }
+}
