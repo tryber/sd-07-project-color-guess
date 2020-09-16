c7f2c812d5b569d296ca13bc7d04ef6a544e5438
moacyrrln <moacyrrln@gmail.com>
2020-09-16 17:29:22 -0300

    200916172922

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("rgb-color").innerText = "("+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
+
+console.log(Math.random*255);
