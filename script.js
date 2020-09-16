2c330b5053b7f71bf98b1224a7d66b2b74adf933
Pedro MF <pedromarques1011@gmail.com>
2020-09-16 18:23:57 -0300

    fixed text

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const OUTPUT_COLOR = document.querySelector(".output");
 
-function randomNumber() {
+function randomColor() {
   const maxNumber = 255;
   return Math.ceil(Math.random() * maxNumber);
 }
 
-OUTPUT_COLOR.innerText = `(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
-
-const balls = document.querySelectorAll(".ball");
-for (let index = 0; index < balls.length; index += 1) {
-  balls[index].style.backgroundColor = `rgb(
-    ${randomNumber()},
-    ${randomNumber()},
-    ${randomNumber()})`;
+function defineBallsColor() {
+  const balls = document.querySelectorAll(".ball");
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].style.backgroundColor = `rgb(
+    ${randomColor()},
+    ${randomColor()},
+    ${randomColor()})`;
+  }
+  const numberofBalls = 6;
+  let randomNumber = Math.floor(Math.random() * numberofBalls);
+  console.log(randomNumber);
+  return balls[randomNumber].style.backgroundColor;
 }
+OUTPUT_COLOR.innerText = defineBallsColor();
