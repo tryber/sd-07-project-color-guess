ac5502fa64f497b05ef97e25f24279c1b9aac155
Carol Bezerra <carol.arbe@gmail.com>
2020-09-16 15:11:49 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function generateColor(number) {
-    const red = Math.floor(Math.random() * number);
-    const green = Math.floor(Math.random() * number);
-    const blue = Math.floor(Math.random() * number);
-    return `(${red}, ${green}, ${blue})`
+  const red = Math.floor(Math.random() * number);
+  const green = Math.floor(Math.random() * number);
+  const blue = Math.floor(Math.random() * number);
+  return `(${red} , ${green} , ${blue})`;
 }
 
-let theMainColor = document.querySelector('#rgb-color');
+const theMainColor = document.querySelector('#rgb-color');
 theMainColor.innerText = generateColor(255);
