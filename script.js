132a7beb2de69c350aa8cc49c586358762d3d307
Andre Serudo <andreserudo@gmail.com>
2020-09-16 10:49:17 -0400

    criacao de cores dinamicas e cor a adivinhar

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+function getNumber(number){
+  return Math.floor(Math.random() * Math.floor(number));
+}
+
+function generateColors(){
+  let colors = [];
+
+  for(let i = 0; i < 6; i += 1){
+    let color = `rgb(${getNumber(255)} , ${getNumber(255)}, ${getNumber(255)})`;
+
+    colors.push(color);
+  }
+
+  return colors;
+}
+
+function pickColor(colors){
+  let index = getNumber(6);
+  let colorToGuess;
+  let auxName;
+
+  auxName = colors[index].split('rgb');
+  colorToGuess = auxName[1];
+
+  return colorToGuess;
+}
+
+function newGame(){
+  let balls = document.querySelectorAll('.ball');
+  let textColorToGuess = document.querySelector('#rgb-color');
+  let colors = generateColors();
+  let colorToGuess = '';
+
+  console.log(colors);
+  for(let i=0; i < 6; i += 1){
+    //console.log(i);
+    console.log(balls[i]);
+    //console.log(colors[i]);
+  }
+
+  colorToGuess = pickColor(colors);
+  textColorToGuess.innerHTML = colorToGuess;
+  console.log(colorToGuess);
+}
+
+let btnReset = document.getElementById('reset-game');
+
+btnReset.addEventListener('click', newGame);
+
+window.onload = newGame;
