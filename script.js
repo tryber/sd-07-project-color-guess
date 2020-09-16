95e72c2cfd994011aab8676776597fc69376190a
Carol Bezerra <carol.arbe@gmail.com>
2020-09-16 16:07:46 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ function generateColor(number) {
   const red = Math.floor(Math.random() * number);
   const green = Math.floor(Math.random() * number);
   const blue = Math.floor(Math.random() * number);
-  return `(${red} , ${green} , ${blue})`;
+  return `(${red}, ${green}, ${blue})`;
 }
 
 const writeRGB = document.querySelector('#rgb-color');
-let theMainColor = generateColor(255);
+const theMainColor = generateColor(255);
 writeRGB.innerHTML = theMainColor;
 
+const allBalls = document.querySelectorAll('.ball');
 function coloringBalls() {
-  const allBalls = document.querySelectorAll('.ball');
-  allBalls[0].style.backgroundColor = `rgb${theMainColor}`
+  allBalls[0].style.backgroundColor = `rgb${theMainColor}`;
   for (let i = 1; i < 6; i += 1) {
     allBalls[i].style.backgroundColor = `rgb${generateColor(255)}`;
   }
 }
-coloringBalls();
 
+window.onload = coloringBalls();
+
+for (let i = 0; i < 6; i += 1) {
+  allBalls[i].addEventListener('click', function () {
+    if (allBalls[i].style.backgroundColor === `rgb${theMainColor}`) {
+      document.querySelector('#answer').innerText = 'Acertou!';
+    } else {
+      document.querySelector('#answer').innerText = 'Errou! Tente novamente!';
+    }
+  });
+};
