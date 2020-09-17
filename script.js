102a981798434371da8feed4b55cf222c7d3ed06
Pedro MF <pedromarques1011@gmail.com>
2020-09-16 23:13:38 -0300

    terminando oq falta no code climate

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const OUTPUT_COLOR = document.querySelector('.output');
 const BALLS = document.querySelectorAll('.ball');
 let PONTUATION = 0;
 
-defineBallsColor();
-for (let i = 0; i < BALLS.length; i += 1) {
-  BALLS[i].addEventListener('click', isChoosenRight);
-}
-document.querySelector('.res-gam').addEventListener('click', defineBallsColor);
-
 function randomColor() {
   const maxNumber = 255;
   return Math.ceil(Math.random() * maxNumber);
+}
+
+function DefineRightOption(numberofBalls) {
+  const randomNumber = Math.floor(Math.random() * numberofBalls);
+  OUTPUT_COLOR.innerText = BALLS[randomNumber].style.backgroundColor;
 }
 
 function defineBallsColor() {
@@ -25,12 +24,6 @@ function defineBallsColor() {
   DefineRightOption(6);
 }
 
-function DefineRightOption(numberofBalls) {
-  let randomNumber = Math.floor(Math.random() * numberofBalls);
-  console.log(randomNumber);
-  OUTPUT_COLOR.innerText = BALLS[randomNumber].style.backgroundColor;
-}
-
 function isChoosenRight(event) {
   if (event.target.style.backgroundColor === OUTPUT_COLOR.innerText) {
     document.querySelector('.resp').innerHTML = 'Acertou!';
@@ -40,3 +33,9 @@ function isChoosenRight(event) {
     document.querySelector('.resp').innerHTML = 'Errou! Tente novamente!';
   }
 }
+
+defineBallsColor();
+for (let i = 0; i < BALLS.length; i += 1) {
+  BALLS[i].addEventListener('click', isChoosenRight);
+}
+document.querySelector('.res-gam').addEventListener('click', defineBallsColor);
