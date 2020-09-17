bf83698e00d9b00fb8dabbb188cee81c70a7217c
moacyrrln <moacyrrln@gmail.com>
2020-09-17 14:26:00 -0300

    200917142600

--- script.js
+++ script.js
@@ -5,11 +5,47 @@ score.innerText = "Placar: " + points;
 buttonreset.addEventListener("click",reset);
 reset();
 function reset() {
+
+    let balls = document.querySelectorAll("#balls");
+    balls[0].removeChild(balls[0].firstElementChild);
+    balls[0].removeChild(balls[0].firstElementChild);
+    balls[0].removeChild(balls[0].firstElementChild);
+    balls[0].removeChild(balls[0].firstElementChild);
+    balls[0].removeChild(balls[0].firstElementChild);
+    balls[0].removeChild(balls[0].firstElementChild);
+    
+    balls = document.getElementById("balls");
+
+    let aux = document.createElement("div");
+    aux.className = "ball";
+    balls.appendChild(aux);
+
+    aux = document.createElement("div");
+    aux.className = "ball";
+    balls.appendChild(aux);
+
+    aux = document.createElement("div");
+    aux.className = "ball";
+    balls.appendChild(aux);
+
+    aux = document.createElement("div");
+    aux.className = "ball";
+    balls.appendChild(aux);
+
+    aux = document.createElement("div");
+    aux.className = "ball";
+    balls.appendChild(aux);
+
+    aux = document.createElement("div");
+    aux.className = "ball";
+    balls.appendChild(aux);
+
     document.getElementById("answer").innerText = "Escolha uma cor";
     let colorrandom = "("+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+", "+parseInt(Math.random()*255)+")";
     document.getElementById("rgb-color").innerText = colorrandom;
     document.getElementById("rgb-color").style.backgroundColor = "rgb"+colorrandom;
-    let balls = document.getElementsByClassName("ball");
+
+    balls = document.getElementsByClassName("ball");
     let random = parseInt(Math.random()*6);
     for (let i = 0;i < balls.length;i++) {
         if (i != random) {
@@ -27,4 +63,5 @@ function reset() {
             }
         });
     }
+
 }
