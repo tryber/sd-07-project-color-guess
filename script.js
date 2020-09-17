34b7ea3f82e9154a9dfbad9b04a8a4f7249807dc
vanderson-henrique <vhbenedito@gmail.com>
2020-09-17 13:59:14 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -3,5 +3,33 @@ function numberRGB() {
     return number;
   }
 
-let rgbColor = document.querySelector('p');
+let rgbColor = document.querySelector('#rgb-color');
 rgbColor.textContent = `(${numberRGB()}, ${numberRGB()}, ${numberRGB()})`;
+
+let answer = document.querySelector('#answer');
+answer.textContent = 'Escolha uma cor';
+
+function resposta(ballClick){
+    let propBackgroundColor = window.getComputedStyle(ballClick.target).getPropertyValue('background-color');
+    console.log(propBackgroundColor);
+    if (propBackgroundColor = rgbColor.textContent){
+        answer.textContent = 'Acertou!';
+    } else {
+        answer.textContent = 'Errou! Tente novamente!';
+    }
+}
+
+
+let balls = document.querySelectorAll('.ball');
+for (let index = 0; index < balls.length; index += 1){
+    balls[index].addEventListener('click', resposta);
+}
+
+window.onload = function() {
+    let balls = document.querySelectorAll('.ball');
+    balls[0].style.backgroundColor = `rgb${rgbColor.textContent}`;
+    for (let index = 1; index < balls.length; index += 1){
+        let colorBall = balls[index];
+      colorBall.style.backgroundColor = `rgb(${numberRGB()} ,${numberRGB()} ,${numberRGB()})`;
+    }
+}
