7d44afc596c6a9504a126e77d98a48c5dd639e97
Ana Karine <anakarine.ti@gmail.com>
2020-09-19 19:55:02 -0300

    req 6: botao reset

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ function rgbGenerator() {
   return `(${x}, ${y}, ${z})`;
 } 
 
+function rgbSelectText() {
+  var rgbText = document.querySelector('#rgb-color');
+  rgbText.innerHTML = rgbGenerator();
+}
 
-var rgbText = document.querySelector('#rgb-color');
-rgbText.innerHTML = rgbGenerator(); 
 
 
 // https://github.com/tryber/sd-04-block5-project-color-guess/pull/17/files
 function rgbSelectBall() {
   const rgbBall = document.querySelectorAll('.ball');
-  rgbBall[Math.round(Math.random() * 5)].style.backgroundColor = `rgb${rgbText.innerHTML}`;
+  rgbBall[Math.round(Math.random() * 5)].style.backgroundColor = `rgb${rgbSelectText()}`;
 }
 
 
@@ -26,6 +28,15 @@ function rgbBalls() {
 }
 rgbBalls();
 // -------------------------------------------------------------
+
+
+function resetGame() {
+  rgbSelectText();
+  rgbBalls();
+  answer.innerHTML = 'Escolha uma cor';
+}
+
+
 
 /*function check () {
   let displayText = document.querySelector('#answer');
