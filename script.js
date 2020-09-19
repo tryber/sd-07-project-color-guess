3fb5a7ee8fcdd6c957609858ee7d01277e2c879e
Ana Karine <anakarine.ti@gmail.com>
2020-09-19 15:31:34 -0300

    req 4: mensagens ao clicar nas bolas

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const x = Math.floor(Math.random() * 256);
 const y = Math.floor(Math.random() * 256);
 const z = Math.floor(Math.random() * 256);
-const rgbGenerator = `RGB (${x}, ${y}, ${z})`;
+const rgbGenerator = `rgb (${x}, ${y}, ${z})`;
 
 const rgbText = document.querySelector('#rgb-color');
 rgbText.innerHTML = rgbGenerator; 
 
 
-
-
+const balls = document.querySelectorAll('.ball');
+const displayText = document.querySelector('#answer');
+if ( balls[i].style.backgroundColor == rgbGenerator) {
+  displayText.innerHTML = 'Acertou!';
+} else {
+  displayText.innerHTML = 'Errou! Tente novamente!';
+}
