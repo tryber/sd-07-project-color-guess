f55ddb92304b34c566a49fcbca955900523a31f5
ericvini <ericcobain3@gmail.com>
2020-09-19 16:50:08 -0300

    adicionei as bolas para que as suas cores sejam adivinhadas

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const textRgb = document.getElementById('rgb-color');
 textRgb.innerText='(168, 34 ,1)';
+
+const containerBall = document.getElementById('container-balls');
+const ball = containerBall.getElementsByTagName('figure');
+
