46dbbf7da6cd818492918ef1f30148941fcd771d
vanderson-henrique <vhbenedito@gmail.com>
2020-09-19 15:54:38 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ function numberRGB() {
     const number = Math.floor(Math.random() * 255);
     return number;
   }
-
-let rgbColor = document.querySelector('#rgb-color');
-rgbColor.textContent = `(${numberRGB()}, ${numberRGB()}, ${numberRGB()})`;
-
+let rgbColor;
 let answer = document.querySelector('#answer');
 answer.textContent = 'Escolha uma cor';
 
@@ -25,11 +22,20 @@ for (let index = 0; index < balls.length; index += 1){
     balls[index].addEventListener('click', resposta);
 }
 
-window.onload = function() {
+window.onload = iniciaJogo();
+
+function iniciaJogo() {
+    rgbColor = document.querySelector('#rgb-color');
+    rgbColor.textContent = `(${numberRGB()}, ${numberRGB()}, ${numberRGB()})`;
     let balls = document.querySelectorAll('.ball');
     balls[0].style.backgroundColor = `rgb${rgbColor.textContent}`;
     for (let index = 1; index < balls.length; index += 1){
         let colorBall = balls[index];
       colorBall.style.backgroundColor = `rgb(${numberRGB()} ,${numberRGB()} ,${numberRGB()})`;
     }
+    
+    answer.textContent = 'Escolha uma cor';
 }
+
+let buttonReset = document.querySelector('#reset-game');
+buttonReset.addEventListener('click', iniciaJogo);
