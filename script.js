10a3679a089b0fc05c3ba63ed000b56a16c05a58
alvaro <alvarovascon2@gmail.com>
2020-09-19 14:29:49 -0300

    100%

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const balls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
 const resetButton = document.getElementById('reset-game');
 let colorToGuessString = '';
-let score = document.getElementById('score');
+const score = document.getElementById('score');
 score.innerHTML = 0;
 
 function getRandom() {
@@ -23,23 +23,23 @@ function resetColors() {
   answer.innerHTML = 'Escolha uma cor';
 }
 
-function createClickEventOnBall() {
-  for (let index = 0; index < balls.length; index += 1) {
-    balls[index].addEventListener('click', function createEvent (event) {
-      if (event.target.style.backgroundColor === colorToGuessString) {
-        answer.innerHTML = 'Acertou!';
-        previousScore = parseInt(score.innerHTML);
-        score.innerHTML = previousScore + 3;
-        previousScore = 0;
-      } else {
-        answer.innerHTML = 'Errou! Tente novamente!';
-        score.innerHTML = 0;
-      }
-    });
+function createEvent(event) {
+  if (event.target.style.backgroundColor === colorToGuessString) {
+    answer.innerHTML = 'Acertou!';
+    let previousScore = parseInt(score.innerHTML, 10);
+    score.innerHTML = previousScore + 3;
+    previousScore = 0;
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!';
+    score.innerHTML = 0;
   }
 }
 
-
+function createClickEventOnBall() {
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].addEventListener('click', createEvent);
+  }
+}
 
 function initialize() {
   resetColors();
