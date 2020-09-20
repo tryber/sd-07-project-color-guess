fa7d212b9c1ae2fc7a2975b3e8a85e2ab5c7fafb
marcelosoliveira <msbobsk8@gmail.com>
2020-09-20 14:15:16 -0300

    code climate color-guess

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function geraCores() {
-  let r = Math.round(Math.random() * 255);
-  let g = Math.round(Math.random() * 255);
-  let b = Math.round(Math.random() * 255);
-  let rgb = `${r}, ${g}, ${b}`;
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  const rgb = `rgb(${r}, ${g}, ${b})`;
   return rgb;
 }
 
 const ball = document.querySelectorAll('.ball');
 for (let index = 0; index < ball.length; index += 1) {
-  ball[index].style.backgroundColor = "rgb("+geraCores()+")";
+  ball[index].style.backgroundColor = geraCores();
 }
 
 const rgbColor = document.querySelector('#rgb-color');
-let igualcores = ball[Math.round(Math.random() * (ball.length - 1))].style.backgroundColor;
+const igualcores = ball[Math.round(Math.random() * (ball.length - 1))].style.backgroundColor;
 rgbColor.innerText = igualcores;
 
-let placar = document.querySelector('#score');
+const placar = document.querySelector('#score');
 let acerto = 0;
 
 for (let index = 0; index < ball.length; index += 1) {
-  ball[index].addEventListener('click', () => {
+  ball[index].addEventListener('click', function () {
     if (rgbColor.innerText === ball[index].style.backgroundColor) {
       document.querySelector('#answer').innerText = 'Acertou! Novas cores!';
       acerto += 3;
@@ -36,19 +36,10 @@ for (let index = 0; index < ball.length; index += 1) {
 
 const botaoJogar = document.querySelector('#reset-game');
 botaoJogar.addEventListener('click', () => {
-  const ball = document.querySelectorAll('.ball');
   for (let index = 0; index < ball.length; index += 1) {
-    ball[index].style.backgroundColor = "rgb("+geraCores()+")";
+    ball[index].style.backgroundColor = geraCores();
   }
-  const rgbColor = document.querySelector('#rgb-color');
   let igualcores = ball[Math.round(Math.random() * (ball.length - 1))].style.backgroundColor;
   rgbColor.innerText = igualcores;
-  document.querySelector('#answer').innerText = 'Escolha uma cor'
+  document.querySelector('#answer').innerText = 'Escolha uma cor';
 });
-
-
-
-
-
-
-
