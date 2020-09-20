48227a0450b94e4ca96c7d909d282d0cc816bf87
marcelosoliveira <msbobsk8@gmail.com>
2020-09-20 15:18:15 -0300

    code climate color-guess

--- script.js
+++ script.js
@@ -12,23 +12,25 @@ const placar = document.querySelector('#score');
 let acerto = 0;
 
 for (let index = 0; index < ball.length; index += 1) {
-  ball[index].style.backgroundColor = geraCores();
-  const igualcores = ball[Math.round(Math.random() * (ball.length - 1))].style.backgroundColor;
-  rgbColor.innerText = igualcores;
-  ball[index].addEventListener('click', (event) => {
-    if (rgbColor.innerText === ball[index].style.backgroundColor) {
-      document.querySelector('#answer').innerText = 'Acertou! Novas cores!';
-      acerto += 3;
-      placar.innerText = `Placar: ${acerto}`;
-    } else {
+    ball[index].style.backgroundColor = geraCores();
+    const igualcores = ball[Math.round(Math.random() * (ball.length - 1))].style.backgroundColor;
+    rgbColor.innerText = igualcores;
+    ball[index].addEventListener('click', placarAcertos); 
+  }
+
+function placarAcertos(event) {
+  if (rgbColor.innerText === event.target.style.backgroundColor) {
+    document.querySelector('#answer').innerText = 'Acertou! Novas cores!';
+    acerto += 3;
+    placar.innerText = `Placar: ${acerto}`;
+  } else {
       document.querySelector('#answer').innerText = 'Errou! Tente novamente';
       if (acerto > 0) {
         acerto -= 1;
         placar.innerText = `Placar: ${acerto}`;
       }
-    }
-  });
-}
+  }
+};
 
 const botaoJogar = document.querySelector('#reset-game');
 botaoJogar.addEventListener('click', () => {
