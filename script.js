b9e2dddcbba4ba5c6c644108886953f18b8531db
Ana Karine <anakarine.ti@gmail.com>
2020-09-20 15:43:46 -0300

    ajuste requisito 4

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
+var rgbText = document.querySelector('#rgb-color');
+var answer = document.querySelector('#answer');
+
+
 function rgbGenerator() {
   let x = Math.floor(Math.random() * 256);
-  let y = Math.round(Math.random() * 256);
-  let z = Math.round(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
   return `(${x}, ${y}, ${z})`;
-} 
+}
 
-function rgbSelectText() {
-  var rgbText = document.querySelector('#rgb-color');
+
+function rgbInputText() {
   rgbText.innerHTML = rgbGenerator();
 }
 
 
-
-// https://github.com/tryber/sd-04-block5-project-color-guess/pull/17/files
-function rgbSelectBall() {
-  const rgbBall = document.querySelectorAll('.ball');
-  rgbBall[Math.round(Math.random() * 5)].style.backgroundColor = `rgb${rgbSelectText()}`;
+function rgbSetBall() {
+  let rgbBalls = document.querySelectorAll('.ball');
+  rgbBalls[Math.floor(Math.random() * 5)].style.background = `rgb${rgbText.innerHTML}`;
 }
 
 
-function rgbBalls() {
-  const rgbBall = document.querySelectorAll('.ball');
-  rgbBall.forEach((ball) => {
+function rgbColorBalls() {
+  let balls = document.querySelectorAll('.ball');
+  balls.forEach((ball) => {
     ball.style.backgroundColor = `rgb${rgbGenerator()}`;
   });
-  rgbSelectBall();
+  rgbSetBall();
 }
-rgbBalls();
-// -------------------------------------------------------------
+
+
+// https://github.com/tryber/sd-04-block5-project-color-guess/pull/17/files
+function checkColor() {
+   let balls = document.querySelectorAll('.ball');
+   balls.forEach((ball) => {
+     ball.addEventListener('click', () => {
+       if (ball.style.background === `rgb${rgbText.innerHTML}`) {
+         answer.innerHTML = 'Acertou!';
+       } else {
+         answer.innerHTML = 'Errou! Tente novamente!';
+       }
+     });
+   });
+ }
 
 
 function resetGame() {
-  rgbSelectText();
-  rgbBalls();
+  rgbInputText();
+  rgbColorBalls();
   answer.innerHTML = 'Escolha uma cor';
 }
 
 
-
-/*function check () {
-  let displayText = document.querySelector('#answer');
-  for (let index = 0; index < rgbBall.length; index += 1) {
-    if (rgbBall[index].style.backgroundColor == rgbGenerator()) {
-      displayText.innerHTML = 'Acertou!';
-    } else {
-      displayText.innerHTML = 'Errou! Tente novamente!';
-    }
-  }
-}*/
+rgbInputText();
+rgbColorBalls();
+checkColor();
+resetGame();
 
