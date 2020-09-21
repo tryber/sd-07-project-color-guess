1a9d62966a1bb090f70c4aac54ba68dc6f1f73ae
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-21 19:45:56 -0300

    commit: 0.0.4

---
+++ script.js
@@ -0,0 +1,20 @@
+const balls = document.querySelectorAll('.ball');
+
+function generationRandomColorRGB() {
+  const maxNumberRGB = 256;
+  const decimalNumber = () => Math.floor(Math.random() * maxNumberRGB);
+  return `rgb(${decimalNumber()},${decimalNumber()},${decimalNumber()})`;
+}
+
+window.onload = function () {
+  balls.forEach((ball) => {
+    ball.style.backgroundColor = generationRandomColorRGB();
+  });
+}
+
+document.querySelector('.div-container').addEventListener('click', function (event) {
+  const rgbColor = document.getElementById('rgb-color');
+  const ballBackgroundColor = event.target.style.backgroundColor;
+  if (ballBackgroundColor === '') alert('clique na bola')
+  console.log(ballBackgroundColor);
+});
