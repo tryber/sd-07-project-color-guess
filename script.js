606825bde65f498b9b6b94ff49f085d81f448f26
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-23 01:14:27 -0300

    Requisito 04, 05 - 22/09/2020

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
-const colorsCircle = document.querySelector("#color-palete");
 let numberColors = 6;
 
-function createBalls () {
-  
-  for(let i=0; i<numberColors; i+= 1) {
+function createBalls() {
+  const colorsCircle = document.querySelector("#color-palete");
+  for (let i = 0; i < numberColors; i += 1) {
     let div = document.createElement("div");
     div.classList.add("ball");
     colorsCircle.appendChild(div);
   }
 }
 
+function colorRandomBalls() {
+  const balls = document.querySelectorAll(".ball");
+
+  for (let i = 0; i < numberColors; i += 1) {
+    let randomNumber = Math.round(Math.random() * 1000000);
+    let randomColor = `#${randomNumber}`;
+
+    balls[i].style.backgroundColor = randomColor;
+    // console.log(balls[0])
+  }
+}
+
+function myQuestion() {
+  const question = document.querySelector("#rgb-color");
+
+  let randomNumber1 = Math.round(Math.random() * 255);
+  let randomNumber2 = Math.round(Math.random() * 255);
+  let randomNumber3 = Math.round(Math.random() * 255);
+  
+  let numberQuestion = `(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+  question.innerText = numberQuestion;
+
+  const balls = document.querySelectorAll(".ball");
+  let randomNumber = Math.round(Math.random()*5);
+
+  balls[randomNumber].style.backgroundColor = "rgb"+ question.innerHTML;
+  console.log(randomNumber);
+}
+
+function message() {
+  const answer = document.querySelector("#answer");
+  const balls = document.querySelectorAll(".ball");
+  const question = document.querySelector("#rgb-color");
+  answer.innerText = "Escolha uma cor";
+
+  for(let i = 0; i < balls.length; i += 1) {
+    balls[i].addEventListener("click", function (event) {
+      let divBall = event.target;
+      if (divBall.style.backgroundColor == `rgb${question.innerText}`) {
+        answer.innerText = "Acertou!";
+      } else {
+        answer.innerText = "Errou! Tente novamente!";
+      }
+      console.log(divBall);
+    })
+  }
+  // console.log(balls[0]);
+}
 
 window.onload = function () {
   createBalls();
-}
+  colorRandomBalls();
+  myQuestion();
+  message();
+};
