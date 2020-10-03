fe2ab75f953a601d9cfa79390dba15b658a3823b
Fernando Antunes <fernando.antunes1@gmail.com>
2020-10-02 22:32:02 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 const balls = document.getElementsByClassName('ball');
+const rgbTip = document.getElementById('rgb-color');
+const resetButton = document.getElementById('reset-game');
+const resposta = document.getElementById('answer');
+let guessRGB = [];
 
 document.querySelectorAll('.ball').forEach((item) => {
   item.addEventListener('click', () => {
@@ -11,9 +15,20 @@ const randonRGB = () => {
   const corR = parseInt(((Math.random()) * 256), 0);
   const corG = parseInt(((Math.random()) * 256), 0);
   const corB = parseInt(((Math.random()) * 256), 0);
+  guessRGB.push(`(${corR},${corG},${corB})`);
   return `rgb(${corR},${corG},${corB})`;
 };
 
-for (let index = 0; index < balls.length; index += 1) {
-  balls[index].style.backgroundColor = randonRGB();
-}
+const colorBalls = () => {
+  guessRGB = [];
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].style.backgroundColor = randonRGB();
+  }
+  rgbTip.innerText = guessRGB[parseInt(((Math.random()) * 6), 0)];
+  resposta.innerText = 'Escolha uma cor';
+  // console.log(guessRGB);
+};
+
+resetButton.addEventListener('click', colorBalls);
+
+colorBalls();
