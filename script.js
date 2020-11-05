cd65493072283a1c3bcdfcbb306cfd4e3076e5d5
cezar88augusto <cezar88augusto@gmail.com>
2020-11-05 11:03:43 -0300

    Refatoraçao

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const balls = document.querySelectorAll('.ball');
 const clearButton = document.getElementById('reset-game');
 const rgbColor = document.getElementById('rgb-color');
@@ -20,35 +21,36 @@ const drawNumber = () => {
 
 const sortedColor = () => {
   const number = drawNumber();
-  const color = document.querySelectorAll('.ball')[number].style.backgroundColor.toString().split('rgb')[1];
+  const color = document.querySelectorAll('.ball')[number].style.backgroundColor.split('rgb')[1];
   rgbColor.innerText = color;
 };
 
-const addSelectedClass = (event) => {
+const setColor = () => {
+  balls.forEach((ball) => {
+    ball.style.backgroundColor = randomColor();
+  });
+  sortedColor();
+  answer.innerText = 'Escolha uma cor';
+};
+
+const getAnswerAndGenerateNewColors = (event) => {
   if (event.target.style.backgroundColor === `rgb${document.getElementById('rgb-color').innerText}`) {
+    setColor();
     answer.innerText = 'Acertou!';
     scoreElement.innerText = `Placar: ${score += 3}`;
   } else {
+    setColor();
     answer.innerText = 'Errou! Tente novamente!';
   }
 };
 
 const getColor = () => {
   for (let index = 0; index < balls.length; index += 1) {
-    balls[index].addEventListener('click', addSelectedClass);
+    balls[index].addEventListener('click', getAnswerAndGenerateNewColors);
   }
-};
-
-const setColor = () => {
-  balls.forEach((element) => {
-    element.style.backgroundColor = randomColor();
-  });
-  sortedColor();
-  answer.innerText = 'Escolha uma cor';
 };
 
 clearButton.addEventListener('click', setColor);
 
 getColor();
 setColor();
-sortedColor();
